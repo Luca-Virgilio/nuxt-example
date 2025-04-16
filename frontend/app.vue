ae9f74e6460d7346835c42b032e53c384eef3137
Luca Virgilio <l.virgilio@reply.it>
2025-04-16 23:36:28 +0200

    Create layout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <NuxtPage></NuxtPage>
-    <div
-      class="fixed bottom-0 left-0 right-0 bg-gray-800 text-white text-center p-4"
-    >
-      <p>Footer Content</p>
-    </div>
+    <NuxtLayout>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
   </div>
 </template>
